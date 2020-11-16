18f86581042f78ee72d1e9657e2b996661d361a4
jmoho <jmoho@gsitm.com>
2020-11-16 09:10:34 +0900

    First Project

---
+++ myprofile/project/js/myfirstjs.js
@@ -0,0 +1,6 @@
+var menuHeight = document.querySelector(".menu").offsetHeight;
+var location = document.querySelector("#move").offsetTop;
+
+function textchange() { 
+document.getElementById("change").innerHTML = "Hello JavaScript!";
+} 
