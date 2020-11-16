db82cf43253e504e9df43a02c0baf3ead3d9f4cc
jmoho <jmoho@gsitm.com>
2020-11-16 09:09:45 +0900

    no message

--- myprofile/project/js/myfirstjs.js
+++
@@ -1,6 +0,0 @@
-var menuHeight = document.querySelector(".menu").offsetHeight;
-var location = document.querySelector("#move").offsetTop;
-
-function textchange() { 
-document.getElementById("change").innerHTML = "Hello JavaScript!";
-} 
