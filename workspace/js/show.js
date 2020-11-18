1665f1f8f9787ab5195b600cb90e5e5a566708f8
jmoho <jmoho@gsitm.com>
2020-11-19 08:33:36 +0900

    no message

---
+++ workspace/js/show.js
@@ -0,0 +1,6 @@
+function writeRe(){
+    var i = document.getElementById("show_form");
+    i.style.display="block";
+    var j = document.getElementById("reviewDe");
+    j.style.display="none";
+}
