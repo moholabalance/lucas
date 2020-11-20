a31b69341e346c839982f2683b9beeae9d533229
jmoho <jmoho@gsitm.com>
2020-11-20 17:51:06 +0900

    no message

--- workspace/js/clickmotion.js
+++ workspace/js/clickmotion.js
@@ -1,3 +1,11 @@
 function showalert(){
       alert("회원가입 완료!!!!");
 }
+
+function carform(){
+      alert("※허위매물 등록 적발 시 법적대응을 받을 수 있습니다.※");
+}
+
+function loginin(){
+      alert("환영합니다! GS 래셔널카 만의 특별한 혜택을 누려보세요");
+}
