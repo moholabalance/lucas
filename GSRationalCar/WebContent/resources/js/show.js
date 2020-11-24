90c38caf3f07cfc17187bc54f0dbfc23d7103b12
jmoho <jmoho@gsitm.com>
2020-11-24 11:31:34 +0900

    Revert "jsp"
    This reverts commit 6b1e64351f421b622fe13584db601e9806efeccb.

--- GSRationalCar/WebContent/resources/js/show.js
+++
@@ -1,6 +0,0 @@
-function writeRe(){
-    var i = document.getElementById("show_form");
-    i.style.display="block";
-    var j = document.getElementById("reviewDe");
-    j.style.display="none";
-}
